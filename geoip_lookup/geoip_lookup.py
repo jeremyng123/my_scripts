3979e8d09342550cb10c9d2177a95c1d941fb39b
Jeremy Ng <ngyzj95@gmail.com>
2021-12-16 18:25:51 +0800

    updated geoip_lookup so it creates a more robust csv file and checks for registered_country and origin_country

--- geoip_lookup/geoip_lookup.py
+++ geoip_lookup/geoip_lookup.py
@@ -1,28 +1,67 @@
 import geoip2.database
+import pandas as pd
 
 # This reader object should be reused across lookups as creation of it is
 # expensive.
-citycount = 0
-countrycount = 0
-result = "count\tip\t\tcountry\tASN{number}\t{ASN Organisation}\n"
+error_ips = ""
+ips = []
+cities_reg = []
+cities = []
+asn_num = []
+ASNs = []
+
+reg_ips = []
+reg_cities_orig = []
+reg_cities = []
+reg_asn_num = []
+reg_ASNs = []
+
 with open('lookup_ips.txt', 'r') as f:
     for ip in f:
         with geoip2.database.Reader("GeoLite2-City.mmdb") as city:
             with geoip2.database.Reader("GeoLite2-ASN.mmdb") as asn:
                 with geoip2.database.Reader(
                         "GeoLite2-Country.mmdb") as country:
-                    City = city.city(ip.strip()).country.iso_code
-                    Asn = asn.asn(ip.strip())
-                    Country = country.country(ip.strip())
-                    if City == "SG" or Country.country.iso_code == "SG":
-                        citycount += 1
-                        result += f"{citycount:<5}:{ip.strip():18}{City:8}ASN{Asn.autonomous_system_number:<8}{Asn.autonomous_system_organization:>30}\n"
+                    try:
+                        City = city.city(ip.strip())
+                        Asn = asn.asn(ip.strip())
+                        Country = country.country(ip.strip())
+                        if City.country.iso_code == "SG" or Country.country.iso_code == "SG" or City.registered_country.iso_code == "SG" or Country.registered_country.iso_code == "SG":
+                            ips.append(ip.strip())
+                            cities.append(City.country.iso_code)
+                            cities_reg.append(City.registered_country.iso_code)
+                            asn_num.append("ASN" +
+                                           str(Asn.autonomous_system_number))
+                            ASNs.append(Asn.autonomous_system_organization)
+                        # if City.registered_country.iso_code == "SG" or Country.registered_country.iso_code == "SG":
+                        #     reg_ips.append(ip.strip())
+                        #     reg_cities.append(City.registered_country.iso_code)
+                        #     reg_cities_orig.append(City.country.iso_code)
+                        #     reg_asn_num.append(
+                        #         "ASN" + str(Asn.autonomous_system_number))
+                        #     reg_ASNs.append(Asn.autonomous_system_organization)
+                    except ValueError:
+                        error_ips += ip + '\n'
+                    except geoip2.errors.AddressNotFoundError:
+                        error_ips += ip + '\n'
 
-                    # if Country.registered_country.iso_code == "SG":
-                    #     countrycount += 1
-                    # # if City == "SG":
-                    # #     print("SG", end='')
-                    # #     print(Country)
-                    # print(citycount, countrycount)
+header = ["ip", "country", "ASN", "ASN Organisation"]
 
-print(result)
+print(City)
+print()
+print(Country)
+df = pd.DataFrame({
+    'IP': ips,
+    "Original_country": cities,
+    "Registered_country": cities_reg,
+    "ASN": asn_num,
+    "ASN Organization": ASNs
+})
+
+pd.set_option("display.max_rows", None, "display.max_columns", None,
+              'display.width', 1100, 'display.colheader_justify', 'center',
+              'display.precision', 3)
+df.to_csv("results.csv")
+print(df)
+
+print("Error IP addresses found:\n" + error_ips)
