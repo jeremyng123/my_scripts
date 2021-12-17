9838d2e7cacb2753733d731da133ad3334b3addd
Jeremy Ng <ngyzj95@gmail.com>
2021-12-17 14:10:52 +0800

    updated geoip and vt

--- VT/ip_lookup.py
+++ VT/ip_lookup.py
@@ -1,26 +1,30 @@
 import requests
 import json
 from dotenv import dotenv_values
+import progressbar
+import pandas as pd
 
 config = dotenv_values(".env")
+headers = {"Accept": "application/json", "x-apikey": config['API_KEY']}
+
 result = "count\tip\t\tcountry\tASN{number}\t{ASN Organisation}\n"
-counts = 0
 lines = 0
 percent = 0
-with open("lookup_ips.txt", 'r') as f:
-    for line in f:
-        lines += 1
-    f.seek(0)
-    for i, ip in enumerate(f):
-        url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip.strip()}"
-        headers = {"Accept": "application/json", "x-apikey": config['API_KEY']}
-        response = requests.request("GET", url, headers=headers)
-        ip_resp = json.loads(response.text)['data']['attributes']
-        if ip_resp['country'] == "SG":
-            counts += 1
-            result += f"{counts:<4}{ip.strip():18}{ip_resp['country']:5}ASN{ip_resp['asn']:<8}{ip_resp['as_owner']:>25}\n"
-        if i % (lines // 100) == 0:
-            percent += 1
-            print(f"{percent}% done")
+MAX_BAR = 30
+with progressbar.ProgressBar(max_value=MAX_BAR) as bar:
+    with open("lookup_ips.txt", 'r') as f:
+        for line in f:
+            lines += 1
+        f.seek(0)
+        for i, ip in enumerate(f):
+            url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip.strip()}"
 
+            response = requests.request("GET", url, headers=headers)
+            print(response.text)
+            ip_resp = json.loads(response.text)['data']['attributes']
+            if ip_resp['country'] == "SG":
+                result += f"{ip.strip():18}{ip_resp['country']:5}ASN{ip_resp['asn']:<8}{ip_resp['as_owner']:>25}\n"
+            if i % (int(round(lines / MAX_BAR, 0))) == 0:
+                percent += 1
+                bar.update(percent)
 print(result)
