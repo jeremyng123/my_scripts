c2ca93d62674fee5d9c31725e54edc57a55196de
Jeremy Ng <ngyzj95@gmail.com>
2022-06-03 09:29:18 +0800

    erm

--- git_related/git_submodule_update.bat
+++ git_related/git_submodule_update.bat
@@ -1,2 +1,5 @@
-@REM update all submodules in this my_script module
+@REM update all submodules in this my_script module if its the first time
 git submodule update --init --recursive
+
+@REM update all submodules
+git submodule update --recursive --remote
