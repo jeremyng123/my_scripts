b94e3f8acff193ea80a175cefe929e825921ac84
Jeremy Ng <ngyzj95@gmail.com>
2023-06-19 22:44:32 +0800

    removed file from my commit history

--- git_related/git_remove_file_from_previous_commits.bat
+++ git_related/git_remove_file_from_previous_commits.bat
@@ -1,1 +1,5 @@
+@REM MAKE SURE YOU USE FORWARD SLASH `/` and not `\`!!! git interprets the slashes differently!
 git filter-branch --force --index-filter "git rm --cached --ignore-unmatch %1" --prune-empty --tag-name-filter cat -- --all
+
+@REM NOTE: DO NOT USE THIS SCRIPT UNLESS U KNOW WHAT U ARE DOING!
+git push origin --force --all
